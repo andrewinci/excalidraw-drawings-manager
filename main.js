ef8f37afb362e28f73c937b3a31abed6cb0ccb43
Andrea Vinci <andrea.vinci@kaluza.com>
2022-06-29 18:18:24 +0100

    add save project functionality

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 class Graphics {
-    constructor() {
+    constructor(onsave) {
         let island = document.getElementsByClassName("Island")[0]
         this.verticalStack = island.childNodes[0]
+        this.onsave = onsave
         this.addTitle()
         this.addSaveProjectContainer()
     }
@@ -16,15 +17,22 @@ class Graphics {
     }
 
     addSaveProjectContainer() {
+        const saveButtonId = "save-project-button";
+        const projectNameId = "project-name"
         let saveItemContainer = document.createElement('div')
         saveItemContainer.style = "display: flex;"
         saveItemContainer.innerHTML = `<div style="position: relative;">
             <div class="color-picker-control-container">
-                <input style="border-radius: 4px;margin-right: 10px;width: auto;" spellcheck="false" class="color-picker-input" aria-label="Canvas background" placeholder="project name">
-                <button>Save</button>
+                <input id="${projectNameId}" style="border-radius: 4px;margin-right: 10px;width: auto;" spellcheck="false" class="color-picker-input" aria-label="Canvas background" placeholder="project name">
+                <button id="${saveButtonId}">Save</button>
             </div>
         </div>`
         this.verticalStack.appendChild(saveItemContainer)
+        document.getElementById(saveButtonId).addEventListener("click", () => {
+            const projectNameInput = document.getElementById(projectNameId)
+            this.onsave(this, projectNameInput.value);
+            projectNameInput.value = null;
+        })
     }
 
     addProject(project) {
@@ -40,19 +48,43 @@ class Graphics {
     }
 }
 
+class ProjectsManager {
+    constructor() {
+        this.PROJECTS_KEY_NAME = "projects";
+        this.load()
+    }
 
-console.log("Loading excalidraw projects plugin...")
-// load projects
-const PROJECTS_KEY_NAME = "projects";
-let projects = JSON.parse(localStorage.getItem(PROJECTS_KEY_NAME))
-if (!projects) {
-    // example projects
-    projects = [{name: "Example 1", content: null},{name: "Example 2", content: null}]
-    localStorage.setItem(PROJECTS_KEY_NAME, JSON.stringify(projects))
+    add(project){
+        this.projects.push(project)
+        this.store()
+    }
+
+    load(){
+        const raw = localStorage.getItem(this.PROJECTS_KEY_NAME);
+        this.projects = raw ? JSON.parse(raw) : [];
+        return this.projects
+    }
+
+    store(){
+        localStorage.setItem(this.PROJECTS_KEY_NAME, JSON.stringify(this.projects))
+    }
+
 }
 
-const graphic = new Graphics()
-projects.forEach(p => {
+
+console.log("Loading excalidraw projects plugin...")
+const pm = new ProjectsManager();
+
+function saveProject(graphic, name) {
+    const content = localStorage.getItem("excalidraw");
+    if (!content) console.error("Content not found!!")
+    const newProject = {name: name, content: content};
+    pm.add(newProject)
+    graphic.addProject(newProject)
+}
+
+const graphic = new Graphics(saveProject)
+pm.load().forEach(p => {
     graphic.addProject(p)
 });
 
